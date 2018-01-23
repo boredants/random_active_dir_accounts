a7747b1ab653d0206d58dad32cc2cdf28038cbf5
boredants <boredants@users.noreply.github.com>
2018-01-23 16:36:11 -0500

    Update GetRandomAccounts.ps1

--- GetRandomAccounts.ps1
+++ GetRandomAccounts.ps1
@@ -4,7 +4,7 @@ Write-Host "`n******************************************************************
 Write-Host "*RETRIEVE A NUMBER OF RANDOM ACCOUNTS FROM AN ORGANIZATIONAL UNIT*"
 Write-Host "******************************************************************"
 
-$numAccounts = Read-Host -Prompt "`nEnter number of accounts to reteive: "
+$numAccounts = Read-Host -Prompt "`nEnter number of accounts to retrieve: "
 $resultsFile = Read-Host -Prompt "`nEnter a name for the CSV results file: "
 $OU = Read-Host -Prompt "`nEnter the name of the OU to search: "
 
