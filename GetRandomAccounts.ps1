ee900889ba1c592f0bcac180c745bb3e71829334
boredants <boredants@users.noreply.github.com>
2018-01-23 16:35:12 -0500

    Add files via upload

---
+++ GetRandomAccounts.ps1
@@ -0,0 +1,16 @@
+Import-Module ActiveDirectory
+
+Write-Host "`n******************************************************************"
+Write-Host "*RETRIEVE A NUMBER OF RANDOM ACCOUNTS FROM AN ORGANIZATIONAL UNIT*"
+Write-Host "******************************************************************"
+
+$numAccounts = Read-Host -Prompt "`nEnter number of accounts to reteive: "
+$resultsFile = Read-Host -Prompt "`nEnter a name for the CSV results file: "
+$OU = Read-Host -Prompt "`nEnter the name of the OU to search: "
+
+#Edit this for your domain
+$OU = "OU="+$OU+",DC=<your>,DC=<domain>,DC=<here>"
+
+Get-ADUser -Filter * -SearchBase $OU -Properties * | select GivenName, Surname, EmailAddress | Sort-Object{Get-Random} |`
+Select -First $numAccounts | ConvertTo-Csv -NoTypeInformation -Delimiter "," | % {$_ -replace '"',''} | Select-Object -Skip 1 |`
+ Set-Content -Path $resultsFile
